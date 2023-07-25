702aaf941961bff05682dda58028a6e0a374d9c0
Ighor <ighortorquato@gmail.com>
2023-07-25 01:29:37 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
-r	"github.com/ighortorquato/GoProgram.git/router"
+	r "github.com/ighortorquato/GoProgram.git/router"
+	"github.com/ighortorquato/GoProgram.git/schemas/config"
 )
 
-func main(){
+var (
+	logger *config.Logger
+)
+
+func main() {
+	logger = config.GetLogger("main")
+	//Initialize configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 
 	//Initialize router
 	r.Initialize()
