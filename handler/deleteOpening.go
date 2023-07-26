e0325fe37c1b9e792c80a0903c088fc60d44c507
Ighor <ighortorquato@gmail.com>
2023-07-26 01:47:08 -0300

    docs: add documentation for Delete Openings

--- handler/deleteOpening.go
+++ handler/deleteOpening.go
@@ -8,6 +8,18 @@ import (
 	"github.com/ighortorquato/GoProgram.git/schemas"
 )
 
+// @BasePath /api/v1
+
+// @Summary Delete opening
+// @Description Delete a new job opening
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Param id query string true "Opening identification"
+// @Success 200 {object} DeleteOpeningResponse
+// @Failure 400 {objetc} ErrorResponse
+// @Failure 404 {object} ErrorResponse
+// @Router /opening [delete]
 func DeleteOpenHandler(ctx *gin.Context) {
 	id := ctx.Query("id")
 	if id == "" {
--- handler/response.go
+++ handler/response.go
@@ -33,3 +33,9 @@ type CreateOpeningResponse struct {
 	Message string                  `json:"message"`
 	Data    schemas.OpeningResponse `json:"data"`
 }
+
+type DeleteOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+
